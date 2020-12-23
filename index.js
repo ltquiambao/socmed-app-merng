e438b3c888ede06095de4d91090b60d0ea8fe439
lorenzo <lorenzotquiambao@gmail.com>
2020-12-22 19:56:25 -0500

    setup server and db

---
+++ index.js
@@ -0,0 +1,25 @@
+// dependency imports
+const { ApolloServer } = require('apollo-server'); // server
+const mongoose = require('mongoose'); // mongoDB
+const debug = require('debug')('index'); // debugging / instead of console logging
+
+// relative imports
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers'); // will default at 'index.js'
+const { MONGODB } = require('./config.js');
+
+// ApolloServer is just using express server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+const connectionString = MONGODB;
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    debug('MongoDB Connected')
+    return server.listen({ port: 5000 })
+  })
+  .then(res => {
+    console.log(`Server running at ${res.url}`);
+  });
